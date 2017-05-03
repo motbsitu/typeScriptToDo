d57b365609140ba43b2bb7abafa3b45fee23d5ec
Julie S. Mike <juliesmike@gmail.com>
2017-05-03 13:48:09 -0500

    compile options, optional parameters, template strings

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # TypeScript Essentials
+Learning TypeScript. Review of ES6 features
+
+## Compile TypeScript
+- manual: tsc <nameoffile>  or
+          tsc -w <nameoffile>  (to keep watch and continually compile as save changes)
+- better automatic:
+  - create tsc config file name tsconfig.json
+  include "complierOptions", etc.
+  - then from command line: tsc -w
+    to watch all files
+
+## ES6
+- Optional parameters: add = value after param name to add
+- Template strings: use back ticks (` `)  and ${ } to add in JS expressions.
---
+++ app.js
@@ -0,0 +1,23 @@
+// let todo: string = "Pick up drycleaning";
+// let name: string = "Julie";
+var container = document.getElementById('container');
+//template strings
+// var todo = {
+//   id: 123,
+//   name: "Pick up drycleaning",
+//   completed: true
+//
+// }
+//
+// var displayName = `Todo# ${todo.id}`
+//
+// container.innerHTML =
+//   `<div todo='${todo.id}' class="list-group-item}">
+//     <i class="${ todo.completed ? "": "hidden" } text-success glyphicon glyphicon-ok"></i>
+//     <span class="name">${todo.name}</span>
+//   </div>`
+//let and const
+for (var x = 0; x <= 5; x++) {
+    var counter = x;
+}
+console.log(counter);
